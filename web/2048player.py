0cf0842e67bc88c181c7ec2a229168c6849dca87
笔记本1 <15009202810@163.com>
2021-11-01 09:46:23 +0800

    update 2048player.py, not it can get the condition of board

--- web/2048player.py
+++ web/2048player.py
@@ -9,12 +9,29 @@ browser = webdriver.Edge()
 browser.get(' https://gabrielecirulli.github.io/2048/')
 htmlElem = browser.find_element_by_tag_name('html')
 
-# automatively play
+# candidate actions
 candidateOperation = [Keys.DOWN, Keys.UP, Keys.LEFT, Keys.RIGHT]
+
 for i in range(10000):
-          htmlElem.send_keys(candidateOperation[i%4])
+          #get board condition
+          try:
+                    boardCondition = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+                    board = browser.find_element_by_css_selector('.tile-container')
+                    chessNum = board.text.split('\n')       #get all num
+                    for chess in range(len(chessNum)):
+                              chessLocation = browser.find_element_by_xpath(f"/html/body/div[1]/div[3]/div[3]/div[{chess+1}]").get_attribute("class").split()[2].split("-")[2:] #get location one by one
+                              boardCondition[int(chessLocation[1])-1][int(chessLocation[0])-1] = int(chessNum[chess])
+          except:
+                    continue
+          print(f'\033[32m {boardCondition} \033[0m')
+          #get score
           score = browser.find_element_by_css_selector('.score-container')
           print(f'\033[31m score:{score.text} \033[0m')
+          #play after decision making
+          '''decision making'''
+          operation  = candidateOperation[i%4]
+          htmlElem.send_keys(operation)
+          #retrain after loose
           try:
                     browser.find_element_by_class_name('retry-button').click()
           except:
